26b883330356780e6508df2422a67091a6f83158
AshwinSampathkumar <ashwintrix@gmail.com>
2019-06-24 17:21:04 +0530

    bootstrap_template

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Sidenav from './components/Sidenav';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Sidenav />
+    </div>
+  );
+}
+
+export default App;
